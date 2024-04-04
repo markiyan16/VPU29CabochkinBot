e2a77192e3627419193274f50fb1b7d09a71c4e4
Markiyan <olegyzva04@gmail.com>
2024-04-04 13:58:24 +0300

    Updated main (Fixed)

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import os
 import logging
+
+from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+
+load_dotenv()
+
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -13,7 +20,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token('TELEGRAM_TOKEN').build()
+    application = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
 
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
